cfcb9d298b41e261623139711684de43f74c9c5b
ZalewskiDamian <damian.github@gmail.com>
2022-04-18 11:04:31 +0200

    add login component

--- src/app/login/Login.jsx
+++ src/app/login/Login.jsx
@@ -1,28 +1,36 @@
-import React from 'react';
 import { Link } from 'react-router-dom';
-
+import loginImage from '../../assets/images/login-image.png';
+import logoIcon from '../../assets/images/logo.svg';
+import { StyledLoginWrapper, StyledLoginImageWrapper, StyledLoginImage, StyledLoginContent, StyledLoginInner, StyledLoginHeader, StyledLogo, StyledLoginHeading, StyledForm, StyledInputGroup, StyledLabel, StyledInput, StyledForgotPassword } from './Login.styles';
+import { Button } from '../../components/index';
 import { AppRoute } from '../../routing/AppRoute.enum';
 
 export const Login = () => {
   return (
-    <>
-      <Link to={AppRoute.home}>Products page</Link>
-      <h2>Login</h2>
-      <form>
-        <div>
-          <label>
-            username:
-            <input name="username" />
-          </label>
-        </div>
-        <div>
-          <label>
-            password:
-            <input name="password" type="password" />
-          </label>
-        </div>
-        <button type="submit">submit</button>
-      </form>
-    </>
+    <StyledLoginWrapper>
+      <StyledLoginImageWrapper>
+        <StyledLoginImage src={loginImage} alt='login' />
+      </StyledLoginImageWrapper>
+      <StyledLoginContent>
+        <StyledLoginHeader>
+          <StyledLogo src={logoIcon} alt='logo' />
+        </StyledLoginHeader>
+        <StyledLoginInner>
+          <StyledLoginHeading>Login</StyledLoginHeading>
+          <StyledForm>
+            <StyledInputGroup>
+              <StyledLabel>Username</StyledLabel>
+              <StyledInput name="username" />
+            </StyledInputGroup>
+            <StyledInputGroup>
+              <StyledLabel>Password</StyledLabel>
+                <StyledInput name="password" type="password" />
+            </StyledInputGroup>
+            <Button type="submit">Log in</Button>
+            <StyledForgotPassword>Forgot password?</StyledForgotPassword>
+          </StyledForm>
+        </StyledLoginInner>
+      </StyledLoginContent>
+    </StyledLoginWrapper>
   );
 };
